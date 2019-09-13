bd228f3cd8a0cabef5968301e597b663d14f2c5e
mohak92 <tamhane.mohak@gmail.com>
2019-09-12 22:52:07 -0400

    added functionality to update score and show active player

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector(".dice").style.display = "none";
 
@@ -30,6 +30,27 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     var diceDOM = document.querySelector(".dice");
     diceDOM.style.display = "block";
     diceDOM.src = "assets/images/dice-" + dice + ".png";
+
+    // 3.Update the round score IF the rolled number was NOT a 1
+    if(dice !== 1){
+        //Add Score
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById("current-0").textContent = 0;
+        document.getElementById("current-1").textContent = 0;
+
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        //document.querySelector(".player-0-panel").classList.remove("active");
+        //document.querySelector(".player-1-panel").classList.add("active");
+
+        document.querySelector(".dice").style.display = "none";
+    }
 });
 
 //document.querySelector("#current-" + activePlayer).textContent = dice;
